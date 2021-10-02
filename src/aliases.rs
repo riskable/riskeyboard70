666192229a44bba3445c3608ffcbae5abdbef1bd
Riskable <riskable@youknowwhat.com>
2021-10-02 12:34:03 -0400

    Updated aliases.rs to reflect the Black Pill's slightly different pin utilization on rev3 of the PCB.

--- src/aliases.rs
+++ src/aliases.rs
@@ -6,15 +6,17 @@ use max7219::connectors::PinConnector;
 use max7219::MAX7219;
 use spi_memory::series25::Flash;
 use stm32f4xx_hal::gpio::gpioa::{PA0, PA1, PA2, PA3, PA4, PA5, PA6, PA7, PA8, PA9, PA10, PA15};
-use stm32f4xx_hal::gpio::gpiob::{PB0, PB1, PB3, PB4, PB5, PB8, PB9, PB12};
+use stm32f4xx_hal::gpio::gpiob::{PB0, PB1, PB3, PB4, PB5, PB8, PB9, PB10, PB12};
 use stm32f4xx_hal::gpio::gpioc::{PC13};
-use stm32f4xx_hal::gpio::{Alternate, Analog, Floating, Input, Output, PushPull, AF5, AF6};
+use stm32f4xx_hal::gpio::{AF5, AF6, Alternate, Analog, Floating, Input, Output, PullUp, PushPull};
 use stm32f4xx_hal::otg_fs::{UsbBus, UsbBusType, USB};
 use stm32f4xx_hal::spi::{NoMiso, NoSck, Spi};
 use usb_device::device::UsbDevice;
 use usbd_hid::hid_class::HIDClass;
 use usbd_serial::SerialPort;
 use crate::ws2812::Ws2812;
+use infrared::receiver::{Receiver, PinInput, Poll};
+use infrared::protocol::*;
 
 // pub type UsbKeyClass = keyberon::Class<'static, UsbBusType, dyn Leds>;
 pub type UsbKeyDevice = UsbDevice<'static, UsbBusType>;
@@ -36,6 +38,9 @@ pub type S2 = PB12<Output<PushPull>>;
 pub type S3 = PA15<Output<PushPull>>;
 pub type EN = DummyPin; // NOTE: We assume the enable pin goes to GND at all times
 
+// Power status pin (goes floating when power is connected)
+pub type POWER = PB10<Input<PullUp>>;
+
 // Relay pin
 pub type RELAY1 = PB1<Output<PushPull>>;
 pub type RELAY2 = PA9<Output<PushPull>>;
@@ -54,7 +59,10 @@ pub type SelectPins = (S0, S1, S2, S3, EN);
 pub type Multiplex = Multiplexer<SelectPins>;
 
 // Infrared receiver
-pub type IRRecv = PB0<Input<Floating>>;
+pub type IRPin = PB0<Input<Floating>>;
+// Couldn't get MultiReceiver working so it's disabled for now...
+// pub type IrReceiver = MultiReceiver5<Rc6, Nec, NecSamsung, Rc5, NecApple, Poll, PinInput<IRPin>>;
+pub type IrReceiver = Receiver<Nec, Poll, PinInput<IRPin>>;
 
 // WS2812B-B LEDs
 pub type SPIWS2812B = Ws2812<Spi<SPI3, (NoSck, NoMiso, PB5<Alternate<AF6>>)>>;
